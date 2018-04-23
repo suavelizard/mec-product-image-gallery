ff389111b7149f55da9aa2f52fc545001ae8fec5
suavelizard <suavelizard@hotmail.com>
2018-04-22 18:22:29 -0700

    Force API SSL

--- server/index.js
+++ server/index.js
@@ -5,15 +5,16 @@ const morgan = require('morgan')
 const app = express()
 const path = require('path')
 const env = require('dotenv').config()
-
+const secure = require('express-force-https')
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(secure)
 
 // Routes
 require('./routes')(app);
 
-const port = process.env.PORT || 8090;
-app.listen(port);
-console.log('server started '+ port);
+const port = process.env.PORT || 8090
+app.listen(port)
+console.log('server started '+ port)
 
