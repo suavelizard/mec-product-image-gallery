9aaa1d8d05754dc3673e7b1e1134268f6b95d288
suavelizard <suavelizard@hotmail.com>
2018-04-22 20:51:57 -0700

    re-enable static files

--- server/routes.js
+++ server/routes.js
@@ -7,10 +7,9 @@ const path = require('path');
 const express = require('express')
 module.exports = app => {
   app.use('/api/products', require('./api/products'));
-
-    // console.log('serving static files', path.join(__dirname + '/../client/dist/static'))
-    // app.use('/static', express.static(path.join(__dirname + '/../client/dist/static')))
-    // app.get('/', function(req, res) {
-    //   res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
-    // })
+    console.log('Serving static files', path.join(__dirname + '/../client/dist/static'))
+    app.use('/static', express.static(path.join(__dirname + '/../client/dist/static')))
+    app.get('/', function(req, res) {
+      res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
+    })
 }
