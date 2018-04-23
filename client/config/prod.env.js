d22b0da1d011f03a1bc0c02ae4a5f0fa3e71138f
suavelizard <suavelizard@hotmail.com>
2018-04-22 18:37:29 -0700

    automatically configure api url

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_URL: '"https://mec-product-image-search.herokuapp.com/api"'
 }
--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://localhost:80/api'
+const API_URL = process.env.API_URL || 'http://localhost:8090/api'
 export default axios.create({
   baseURL: API_URL,
   headers: {
